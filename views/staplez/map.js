40e9c499fea31fac7346ed0b9aeb382574ad1ca3
klml <github@klml.de>
2011-11-29 01:08:38 +0100

    some workaorund tried

--- views/staplez/map.js
+++ views/staplez/map.js
@@ -1,12 +1,12 @@
 function(doc) {
-  if (doc.type == "ticket") {
+  if (doc.type == 'ticket') {
       doc.hierarchy = 1 ;
       emit([doc._id, doc.initdate], doc);
   }
-    if (doc.parents && doc.state != "archive" ) {
+    if (doc.parents && doc.state != 'archive' ) {
      for (parentarm in doc.parents ) {
             for ( distance in doc.parents ) {
-                if ( doc.parents[parentarm] == doc.parents[distance] ) doc.hierarchy = distance * 1 + 2 // "* 1" to get number ??? TODO
+                if ( doc.parents[parentarm] == doc.parents[distance] ) doc.hierarchy = distance * 1 + 2
             };
             emit([doc.parents[parentarm], doc._id, doc.parents, doc.initdate], doc);
         }
