56692c45b3f187d01dafdca595e1616e75d6f27b
klml <github@klml.de>
2011-12-09 22:54:45 +0100

    all list sort by initdate

--- views/all-tickets/map.js
+++ views/all-tickets/map.js
@@ -2,6 +2,6 @@ function(doc) {
   if (doc.title && doc.state != 'archive' ) {
     var _doc =  JSON.parse(JSON.stringify(doc)); 
     _doc.hierarchy = 2 ;
-    emit(_doc._id, _doc);
+    emit(_doc.initdate, _doc);
   }
 }
